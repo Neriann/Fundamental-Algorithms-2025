e4323940f61eae3d267ecfa666489221fa0d9bda
nerian <samslowpokd@gmail.com>
2025-12-10 18:06:32 +0300

    add interactive

--- lab3/binomial_heap/main.c
+++ lab3/binomial_heap/main.c
@@ -3,4 +3,29 @@
 
 #include "binomial_heap.h"
 
-int main() {}
+int main() {
+    BinomialHeap* heap = create_binomial_heap();
+
+
+    int command, value;
+    while (1) {
+        printf("Enter command (0 - exit, 1 - insert, 2 - get_min, 3 - extract_min): ");
+        if (scanf("%d", &command) != 1 || !command) {
+            break;
+        }
+        switch (command) {
+        case 1:
+            printf("Enter value: ");
+            scanf("%d", &value);
+            insert_binomial_heap(&heap, value);
+            break;
+        case 2:
+            printf("%d\n", get_binomial_heap(heap));
+            break;
+
+        case 3:
+            extract_binomial_heap(&heap);
+            break;
+        }
+    }
+}
